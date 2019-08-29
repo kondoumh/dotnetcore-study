5ac0561b3d54a57d653030cb7a08ef641b7fd13a
kondoumh <kondoumh@gmail.com>
2019-08-29 12:33:02 +0900

    add post method

--- WebApiSample/TodoApi/Controllers/TodoController.cs
+++ WebApiSample/TodoApi/Controllers/TodoController.cs
@@ -40,5 +40,13 @@ namespace TodoApi.Controllers
       }
       return todoItem;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+    {
+      _context.TodoItems.Add(item);
+      await _context.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetTodoItem), new TodoItem{ Id = item.Id }, item);
+    }
   }
 }
